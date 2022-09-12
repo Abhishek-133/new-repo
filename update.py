76d749c20545f3edea680a5a487e0b0d4477dc37
Abhishek <abhishekrana4a0@gmail.com>
2022-09-12 14:23:02 +0530

    for registration

---
+++ update.py
@@ -0,0 +1,23 @@
+# creating the hash password
+# import bcrypt
+# password='passwordabc'
+# bytes=password.encode('utf-8')
+# salt=bcrypt.gensalt()
+# hash=bcrypt.hashpw(bytes,salt)
+# print(hash)
+
+import bcrypt
+password =input("Enter database stored value: ")
+bytes=password.encode('utf-8')
+salt=bcrypt.gensalt()
+hash=bcrypt.hashpw(bytes,salt)
+print(hash)
+print("\n")
+userPassword=input("Enter user entered value: ")
+userBytes=userPassword.encode('utf-8')
+salt=bcrypt.gensalt()
+user=bcrypt.hashpw(userBytes,salt)
+print(user)
+result=bcrypt.checkpw(userBytes,hash)
+print("\n")
+print(result)
